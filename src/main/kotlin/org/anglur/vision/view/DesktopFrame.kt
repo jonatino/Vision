ed725c177f0cff4aabc9cd3cfb0749af12cb4693
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-10-23 02:04:26 -0400

    Added copyright

--- src/main/kotlin/org/anglur/vision/view/DesktopFrame.kt
+++ src/main/kotlin/org/anglur/vision/view/DesktopFrame.kt
@@ -1,3 +1,21 @@
+/*
+ *     Vision - free remote desktop software built with Kotlin
+ *     Copyright (C) 2016  Jonathan Beaudoin
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.anglur.vision.view
 
 import javafx.application.Platform.runLater
@@ -21,7 +39,7 @@ class DesktopFrame : View() {
 	
 	val screenViews = ArrayList<ScalableImageView>()
 	
-	val captureConfig = VisionGUI.screengrabber
+	val captureConfig = VisionGUI.captureMode
 	
 	var currentView: ScalableImageView? = null
 		get() = screenViews[captureConfig.currentScreen]
